e2b992dd49624947b2ae2a21b463c02a3abf3878
Uilian Ries <uilianries@gmail.com>
2018-10-31 15:41:17 -0300

    #381 Apply Conan conventions
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
 import os
+from conans import ConanFile, CMake, tools
 
 
 class ProtobufConan(ConanFile):
@@ -11,23 +11,24 @@ class ProtobufConan(ConanFile):
     url = "https://github.com/bincrafters/conan-protoc_installer"
     homepage = "https://github.com/protocolbuffers/protobuf"
     author = "Bincrafters <bincrafters@gmail.com>"
-    description = "protoc is a compiler for protocol buffers definitions files. It can can generate C++, Java " \
-                  "and Python source code for the classes defined in PROTO_FILE."
-    license = "BSD"
+    topics = ("protocol-buffers", "protocol-compiler", "serialization", "rpc")
+    description = ("protoc is a compiler for protocol buffers definitions files. It can can "
+                   "generate C++, Java and Python source code for the classes defined in PROTO_FILE.")
+    license = "BSD-3-Clause"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
     settings = "os_build", "arch_build"
     short_paths = True
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def source(self):
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = "protobuf-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         if self.settings.arch_build == 'x86':
             cmake.definitions['CMAKE_C_FLAGS'] = '-m32'
@@ -35,21 +36,20 @@ class ProtobufConan(ConanFile):
         elif self.settings.arch_build == 'x86_64':
             cmake.definitions['CMAKE_C_FLAGS'] = '-m64'
             cmake.definitions['CMAKE_CXX_FLAGS'] = '-m64'
-        cmake.definitions["CMAKE_INSTALL_LIBDIR"] = "lib"
         cmake.definitions["protobuf_BUILD_TESTS"] = False
         cmake.definitions["protobuf_WITH_ZLIB"] = False
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        # CMake install is not used intentionally - copy only executable file
-        self.copy(pattern='protoc*', src=os.path.join(self.build_subfolder, 'bin'), dst='bin')
-        self.copy(pattern='*.proto', src=os.path.join(self.source_subfolder, 'src'), dst='include')
-        self.copy("LICENSE", dst="licenses", src=self.source_subfolder)
+        # INFO: CMake install is not used intentionally - copy only executable file
+        self.copy(pattern='protoc*', src=os.path.join(self._build_subfolder, 'bin'), dst='bin')
+        self.copy(pattern='*.proto', src=os.path.join(self._source_subfolder, 'src'), dst='include')
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
 
     def package_info(self):
         self.env_info.PATH.append(os.path.join(self.package_folder, 'bin'))
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools, RunEnvironment
-import os
+from conans import ConanFile
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            self.run("protoc --version")
+        self.run("protoc --version", run_environment=True)
