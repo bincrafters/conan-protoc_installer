4f3f7ba2cde30ff4ca5f93ef4ba039cbf97e3228
Uilian Ries <uilianries@gmail.com>
2019-04-23 18:00:56 -0300

    Re-build protobuf with old glibc
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,8 +7,8 @@ from bincrafters import build_shared
 
 
 if __name__ == "__main__":
-
+    docker_entry_script = ".ci/build.sh" if "docker_entry_script" in os.getenv("CONAN_DOCKER_IMAGE", "") else None
     arch = os.environ["ARCH"]
-    builder = build_template_installer.get_builder()
+    builder = build_template_installer.get_builder(docker_entry_script=docker_entry_script)
     builder.add({"os" : build_shared.get_os(), "arch_build" : arch}, {}, {}, {})
     builder.run()
