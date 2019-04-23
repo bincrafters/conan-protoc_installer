6115f9145105a3baab5dc87e1f6fe9275cf39645
Uilian Ries <uilianries@gmail.com>
2019-04-23 18:07:03 -0300

    Build protobuf only for Linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,10 +4,11 @@
 import os
 from bincrafters import build_template_installer
 from bincrafters import build_shared
+from conans import tools
 
 
 if __name__ == "__main__":
-    docker_entry_script = ".ci/build.sh" if "docker_entry_script" in os.getenv("CONAN_DOCKER_IMAGE", "") else None
+    docker_entry_script = ".ci/build.sh" if tools.os_info.is_linux else None
     arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder(docker_entry_script=docker_entry_script)
     builder.add({"os" : build_shared.get_os(), "arch_build" : arch}, {}, {}, {})
